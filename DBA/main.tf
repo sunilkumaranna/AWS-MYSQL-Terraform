329056951ed3ff7a5222799d3b1ec10e4952928f
adinarayanaanna <adinarayanaanna@gmail.com>
2026-02-26 20:01:56 +0530

    added MSSQL rds instance too

--- DBA/main.tf
+++ DBA/main.tf
@@ -11,12 +11,17 @@ terraform {
   }
 }
 
+# ---------------------------
+# VARIABLES
+# ---------------------------
 variable "db_username" { default = "admin" }
 variable "db_name" { default = "mydb2" }
 variable "allocated_storage" { default = 20 }
 variable "instance_class" { default = "db.t3.micro" }
 
-# ---- Secrets Manager ----
+# ---------------------------
+# MYSQL SECRETS
+# ---------------------------
 resource "aws_secretsmanager_secret" "db_password" {
   name = "mysql-db-password"
 }
@@ -30,14 +35,34 @@ locals {
   db_password = jsondecode(aws_secretsmanager_secret_version.db_password_version.secret_string).password
 }
 
-# ---- Subnet Group ----
+# ---------------------------
+# MSSQL SECRETS
+# ---------------------------
+resource "aws_secretsmanager_secret" "mssql_password" {
+  name = "mssql-db-password"
+}
+
+resource "aws_secretsmanager_secret_version" "mssql_password_version" {
+  secret_id     = aws_secretsmanager_secret.mssql_password.id
+  secret_string = jsonencode({ password = "StrongPassword456!" })
+}
+
+locals {
+  mssql_password = jsondecode(aws_secretsmanager_secret_version.mssql_password_version.secret_string).password
+}
+
+# ---------------------------
+# SUBNET GROUP
+# ---------------------------
 resource "aws_db_subnet_group" "rds" {
   name = "rds-subnet-group"
 
   subnet_ids = data.terraform_remote_state.infra.outputs.public_subnet_ids
 }
 
-# ---- RDS Instance ----
+# ---------------------------
+# MYSQL RDS INSTANCE
+# ---------------------------
 resource "aws_db_instance" "mysql" {
   identifier           = "my-mysql-db"
   engine               = "mysql"
@@ -53,7 +78,38 @@ resource "aws_db_instance" "mysql" {
   multi_az             = false
   allow_major_version_upgrade = true
   apply_immediately    = true
+
   db_subnet_group_name = aws_db_subnet_group.rds.name
+
+  vpc_security_group_ids = [
+    data.terraform_remote_state.infra.outputs.rds_sg_id
+  ]
+}
+
+# ---------------------------
+# SQL SERVER EXPRESS RDS INSTANCE
+# ---------------------------
+resource "aws_db_instance" "mssql_express" {
+  identifier           = "my-mssql-express-db"
+  engine               = "sqlserver-ex"
+  engine_version       = "15.00" # SQL Server 2019 Express
+  instance_class       = "db.t3.small" # SQL Server minimum class
+  allocated_storage    = 20
+  storage_type         = "gp2"
+
+  username             = "sa"
+  password             = local.mssql_password
+  db_name              = "master" # SQL Server requires master database
+
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+  storage_encrypted    = true
+  multi_az             = false
+  allow_major_version_upgrade = true
+  apply_immediately    = true
+
+  db_subnet_group_name = aws_db_subnet_group.rds.name
+
   vpc_security_group_ids = [
     data.terraform_remote_state.infra.outputs.rds_sg_id
   ]
