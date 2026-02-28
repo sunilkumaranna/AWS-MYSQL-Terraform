491500fc41d0b9c0155c98302361c45b05cb43f9
adinarayanaanna <adinarayanaanna@gmail.com>
2026-02-28 16:54:23 +0530

    done code changes

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,12 +7,9 @@ output "public_subnet_ids" {
     aws_subnet.public_a.id,
     aws_subnet.public_b.id
   ]
+  description = "List of public subnet IDs for RDS"
 }
 
 output "rds_sg_id" {
   value = aws_security_group.rds.id
-}
-
-output "public_subnet_ids" {
-  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
